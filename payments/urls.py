49bc23abb1daf00e357df9f78fbe951c0a281455
Aditya Dinesh Lakeshri <adityalakeshri6@gmail.com>
2026-09-20 19:44:52 +0530

    Add payment routes for online payment processing

--- payments/urls.py
+++ payments/urls.py
@@ -5,5 +5,7 @@ app_name = 'payments'
 
 urlpatterns = [
     path('order/<int:order_pk>/record/', views.record_payment, name='record'),
+    path('order/<int:order_pk>/pay/', views.pay_online, name='pay_online'),
+    path('order/<int:order_pk>/pay/verify/', views.verify_online_payment, name='verify_online'),
     path('manage/', views.payment_list, name='list'),
 ]
